bcff95e6421fe4f7d55ff6fd6ab863e483bac5fc
Mark Kahoush <51457336+mkahoush-dev@users.noreply.github.com>
2024-01-25 13:07:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -60,13 +60,22 @@ def content_summerizer():
         summary = run(title, length)
     return render_template('content_summerizer.html', summary=summary)
 
+@app.route('/question_image', methods=['GET', 'POST'])
+def question_image():
+    from question_image import run
+    answer = ""
+    if request.method == 'POST':
+        question = request.form.get('question')
+        answer = run(question)
+    return render_template('question_image.html', answer=answer)
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload/<file_id>', methods=['GET', 'POST'])
-def upload_file(file_id):
+@app.route('/upload/<file_id>/<template>', methods=['GET', 'POST'])
+def upload_file(file_id, template):
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
@@ -81,6 +90,9 @@ def upload_file(file_id):
             os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_id))
             # return redirect(url_for('download_file', name=filename))
+        if template =='q':
+            return render_template("question_image.html")
+
     return render_template("style_image.html")
 
 @app.route('/uploads/<name>')
